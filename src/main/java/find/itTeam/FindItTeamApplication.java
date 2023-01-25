90e746cce8f0c6980689c0b18a19152a70c421a1
archerfromRussia <animechnikasn@gmail.com>
2023-01-25 22:17:50 +0300

    Update FindItTeamApplication.java

--- src/main/java/find/itTeam/FindItTeamApplication.java
+++ src/main/java/find/itTeam/FindItTeamApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FindItTeamApplication {
-//1
+
 	public static void main(String[] args) {
 		SpringApplication.run(FindItTeamApplication.class, args);
 	}
